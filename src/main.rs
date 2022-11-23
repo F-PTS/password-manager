954e07592dec9520fe5ef5cd11ca677ad40cac48
Filip Pitus <fp.filipPTS@gmail.com>
2022-11-23 20:08:48 +0100

    adding all possible characters

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let numbers = vec![1..9];
+    let character: Vec<char> = "qwertyuiopasdfghjklzxcvbnm".chars().collect();
+    let special_characters: Vec<char> = "~`!@#$%^&*()_-+={[}]|:;'<,>./?".chars().collect();
+}
